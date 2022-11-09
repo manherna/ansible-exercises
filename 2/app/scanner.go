85da93fde8c3edf8e07e9994ae14e6e817c0bc14
Manuel Hernández <manuel.hna@gmail.com>
2022-11-09 11:45:00 +0100

    Added Network CIDR range parsing

--- 2/app/scanner.go
+++ 2/app/scanner.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
@@ -31,8 +32,27 @@ func main() {
 }
 
 func GetNetworkRange(cidrRange string) []string {
-	//TODO: Implement network ranges parsing
-	return []string{"1.1.1.1", "2.2.2.2", "3.3.3.3", "4.4.4.4"}
+	// convert string to IPNet struct
+	_, ipv4Net, err := net.ParseCIDR(cidrRange)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// convert IPNet struct mask and address to uint32
+	mask := binary.BigEndian.Uint32(ipv4Net.Mask)
+	start := binary.BigEndian.Uint32(ipv4Net.IP)
+	// find the final address
+	finish := (start & mask) | (mask ^ 0xffffffff)
+
+	var result []string
+	// loop through addresses as uint32
+	for i := start; i <= finish; i++ {
+		// convert back to net.IP
+		ip := make(net.IP, 4)
+		binary.BigEndian.PutUint32(ip, i)
+		result = append(result, ip.String())
+	}
+	return result
 }
 
 func ScanAllPorts(ip string) {
